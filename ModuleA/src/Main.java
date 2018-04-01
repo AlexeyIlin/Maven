e4ad34f6c5d1f39a4249409ff1171ba43081260d
Alexey <archon@ua.fm>
2018-04-01 18:12:04 +0300

    commit

---
+++ ModuleA/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Aleksey on 01.04.2018.
+ */
+public class Main {
+    public static void main (String args[]){
+        System.out.println("Module A");
+    }
+}
---
+++ ModuleC/src/ModuleC.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Aleksey on 01.04.2018.
+ */
+public class ModuleC {
+    public static void main (String args[]){
+        System.out.println("Module C");
+    }
+}
